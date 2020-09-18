9a858a931732ffb59342a4341a49d5082c687f07
Lester Mesa <lester.mesa@payrix.com>
2020-09-17 22:44:38 -0400

    Application structure setup and CRUD of Vehicles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VehiclesListComponent } from './components/vehicle/list/vehicles-list.component';
+import { VehicleDetailsComponent } from './components/vehicle/details/vehicle-details.component';
+import { VehicleAddComponent } from './components/vehicle/add/vehicle-add.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'vehicles', pathMatch: 'full' },
+  { path: 'vehicles', component: VehiclesListComponent },
+  { path: 'vehicles/add', component: VehicleAddComponent },
+  { path: 'vehicles/:id', component: VehicleDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { VehicleAddComponent } from './components/vehicle/add/vehicle-add.component';
+import { VehicleDetailsComponent } from './components/vehicle/details/vehicle-details.component';
+import { VehiclesListComponent } from './components/vehicle/list/vehicles-list.component';
+import {FormsModule} from "@angular/forms";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {InterceptorServices} from "./interceptor.interceptor";
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehicleAddComponent,
+    VehicleDetailsComponent,
+    VehiclesListComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorServices,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
